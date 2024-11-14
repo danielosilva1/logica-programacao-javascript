47bb97c53dbc85768c3357255905edfbaf205603
dosilva <danielosilva@alu.ufc.br>
2024-11-13 21:48:42 -0300

    aula 4

---
+++ aula4/game-js/app.js
@@ -0,0 +1,32 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+let numeroTentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+
+    // Se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        numeroTentativas++;
+
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+}
+
+let palavraTentativa = numeroTentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto}) com ${numeroTentativas} ${palavraTentativa}`);
+
+// if (numeroTentativas > 1) {
+//     alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto}) com ${numeroTentativas} tentativas`);
+// } else {
+//     alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto}) com ${numeroTentativas} tentativa`);
+// }
